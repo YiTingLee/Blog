b212004507480f40e76d34bcdac51b274f3a70b7
YiTingLee <ranye0719@gmail.com>
2017-04-28 22:36:16 +0800

    新增react-router-dom,新增Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter , Route} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component{
+  render(){return <div>Hello</div>}
+}
+
+class Good extends React.Component{
+  render(){return <div>Good</div>}
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        Header
+        <Route path="/hi" component={Hello} />
+        <Route path="/gd" component={Good} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));

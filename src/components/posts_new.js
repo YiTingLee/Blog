6e708dabad67d0e3c038226be2683faa0900e1f3
YiTingLee <ranye0719@gmail.com>
2017-05-02 21:19:07 +0800

    add validate

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -15,21 +15,29 @@ class PostsNew extends Component {
           {...field.input}
           type="text"
         />
+        {field.meta.error}
       </div>
     );
   }
 
+  onSubmit(values){
+    // this === component
+    console.log(values);
+  }
+
   render(){
+    const {handleSubmit} = this.props;
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title for Post"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -37,11 +45,32 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className = "btn btn-primary">submit</button>
       </form>
     );
   }
 }
 
+function validate(values){
+
+  const errors ={};
+  // Validate the inputs from 'values'
+  if(!values.title || values.title.length <3 ){
+    errors.title = "Enter a title that is at least 3 characters";
+  }
+  if(!values.categories){
+    errors.categories = "Enter some categories!";
+  }
+  if(!values.content){
+    errors.content = "Enter some content!";
+  }
+
+  //If errors is empty, the form is fine to submit
+  // If errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate:validate,
   form:'PostsNewForm'
 })(PostsNew);

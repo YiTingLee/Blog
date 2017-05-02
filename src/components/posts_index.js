851d0cf6d1472640cf7bdd4ee70f2ff89d1fe5e5
YiTingLee <ranye0719@gmail.com>
2017-05-02 12:26:35 +0800

    add Link Button

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React ,{Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 import _ from 'lodash';
+import {Link} from 'react-router-dom';
 
 class PostsIndex extends Component{
   componentDidMount(){
@@ -22,6 +23,11 @@ class PostsIndex extends Component{
     console.log(this.props.posts);
     return(
       <div>
+        <div className="text-xs-left">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post!
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,15 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class PostsNew extends Component {
   render(){
     return(
       <div>
+        <div className="text-xs-left">
+          <Link className="btn btn-primary" to="/">
+            Back
+          </Link>
+        </div>
         PostsNew!
       </div>
     );

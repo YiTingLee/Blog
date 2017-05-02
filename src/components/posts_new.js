8233ba2fbe3049af4e9af73a85ee64fe14ff412a
YiTingLee <ranye0719@gmail.com>
2017-05-02 13:44:52 +0800

    add form on PostsNew page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,19 +1,47 @@
 import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Field,reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          // onChange={field.input.onChange}
+          // onFocus={field.input.onFocus}
+          // onBlur={field.input.onBlur}
+          className="form-control"
+          {...field.input}
+          type="text"
+        />
+      </div>
+    );
+  }
+
   render(){
     return(
-      <div>
-        <div className="text-xs-left">
-          <Link className="btn btn-primary" to="/">
-            Back
-          </Link>
-        </div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title for Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form:'PostsNewForm'
+})(PostsNew);

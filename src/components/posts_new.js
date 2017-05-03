13babb58f2186c47f7becbb1895d7fc87b58056a
YiTingLee <ranye0719@gmail.com>
2017-05-03 10:44:03 +0800

    add cancel button and add conditional styling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,8 +4,11 @@ import {Field,reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field){
+    // const {meta:{touched , error }} = field;
+    const className =`form-group ${field.meta.touched && field.meta.error ? 'has-danger' : '' }`;
+
     return(
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           // onChange={field.input.onChange}
@@ -15,7 +18,9 @@ class PostsNew extends Component {
           {...field.input}
           type="text"
         />
-        {field.meta.error}
+        <div className="text-help">
+          {field.meta.touched ? field.meta.error : ''}
+        </div>
       </div>
     );
   }
@@ -46,6 +51,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className = "btn btn-primary">submit</button>
+          <Link className="btn btn-danger" to="/">Cancel</Link>
       </form>
     );
   }

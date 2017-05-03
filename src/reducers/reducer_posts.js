1058ba35df4363a3e593c0160919521bb192a915
YiTingLee <ranye0719@gmail.com>
2017-05-03 22:18:52 +0800

    add posts_show.js

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,19 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS,FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {},action){
   switch (action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data,'id');
+      break;
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;
+      // return newState;
+
+      return { ...state,[action.payload.data.id]: action.payload.data };
+      break;
     default:
       return state;
 
